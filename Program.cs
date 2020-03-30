37e34f7103d4f8a4758b09a7d83c6409e90661f4
gokertoplu <gokertoplu@gmail.com>
2020-03-30 12:23:04 +0300

    mysuperbank first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BankLibrary;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Goker", 50000);
+            
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(30000,DateTime.Now, "Kira Odemesi ");
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} Kira Odemesi.");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
